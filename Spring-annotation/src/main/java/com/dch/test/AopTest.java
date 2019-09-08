bd875ebd41777fab9d13e8755e73d981bff1cb8d
ChenghaoDeng@outlook.com <54314484+NBNOWORDS@users.noreply.github.com>
2019-09-08 23:14:34 +0800

    Aop
    Aop

--- Spring-annotation/src/main/java/com/dch/test/AopTest.java
+++ Spring-annotation/src/main/java/com/dch/test/AopTest.java
@@ -31,6 +31,17 @@ import com.dch.config.MyConfigOfAop;
  * AbstractAdvisorAutoProxyCreator.setBeanFactory() ->initBeanFactory()
  * 
  * AspectJAwareAdvisorAutoProxyCreator.initBeanFactory()
+ * 
+ * 	流程
+ * 		1)、传入配置类，创建ioc容器
+ * 		2)、注册配置类，调用refresh()，刷新容器
+ * 		3)、registerBeanPostProcessors(beanFactory);注册bean的后置处理器来方便拦截bean的创建：
+ * 			1)、先获取ioc容器中已经定义了的需要创建对象的所有BeanPostProcessor
+ * 			2)、给容器中添加一些别的BeanPostProcessor
+ * 			3)、优先注册实现了PriorityOrdered接口的BeanPostProcessor
+ * 			4)、再给容器中注册实现了Ordered接口的BeanPostProcessor
+ * 			5)、注册没实现优先级接口的BeanPostProcessor
+ * 			6)、注册BeanPostProcessor，实际上就是创建BeanPostProcessor对象，保存在容器中
  * @author DENGCHENGHAO
  *
  */
